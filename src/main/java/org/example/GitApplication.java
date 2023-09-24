7d955c2d581f5d38713e55a08a62f1923a80164c
leibus <leibus@example.com>
2023-09-24 22:16:47 +0800

    init commit

---
+++ src/main/java/org/example/GitApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GitApplication.class,args);
+    }
+}
